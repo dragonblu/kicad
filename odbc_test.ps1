7c683604bec065df2e888b9e2c0a4dd78bb102d6
dragonblu <dev.hspark@gmail.com>
2023-05-04 11:38:26 +0900

    Test Database

---
+++ odbc_test.ps1
@@ -0,0 +1,12 @@
+$conn = New-Object System.Data.Odbc.OdbcConnection("dsn=KiCAD_SQLite3")
+$conn.Open()
+$cmd = $conn.CreateCommand()
+$cmd.CommandText = "SELECT * FROM Resistors"
+$reader = $cmd.ExecuteReader()
+$reader.Read()
+$reader[0]
+$reader[1]
+$reader[2]
+$reader[3]
+$reader.Close()
+$conn.Close()
